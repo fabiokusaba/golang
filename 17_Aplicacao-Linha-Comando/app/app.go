6b1d23cc1ca39f94afd881dd4c95c32e4fbbe5c9
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-27 21:03:28 -0300

    implementado a funcionalidade de busca por IP

--- 17_Aplicacao-Linha-Comando/app/app.go
+++ 17_Aplicacao-Linha-Comando/app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Esse pacote vai ter a nossa aplicação de linha de comando, tudo vai ser criado aqui
 // Vamos ter uma função Gerar, perceba que aqui usamos o G maiúsculo porque a ideia é que o pacote main importe essa função, então
@@ -13,5 +19,36 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca IPs e Nomes de Servidor na Internet"
 
+	// Comandos da aplicação
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca de IPs de endereços na Internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	// Obtendo o valor da flag 'host'
+	host := c.String("host")
+
+	// Busca por IPs utilizando o pacote net
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Como nossa variável ips é um slice eu posso estar iterando sobre ela
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
